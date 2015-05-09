d1f95a793f0b043ed9ed87baeef726d4e8478b0a
FezodgeIII <j.pullan2000@gmail.com>
2015-05-09 19:44:09 +0100

    Level Loading Fixes/Sword.js Clean Up
    I fixed the bug where the forest levels wouldn't load as you moved
    through the hidden wall. I also cleaned up a bit of the Sword.js file.

--- game/levels/content/items/Sword.js
+++ game/levels/content/items/Sword.js
@@ -7,21 +7,21 @@ module.exports=function(api){
 	this.determiner="a";
 	this.durability=20;
 	this.damage=3;
-	this.on("use", (function(){//item specific event, must be fired from a content file
+	this.on("use", (function(){
 		this.durability-=1;
-	}).bind(this));//event callback functions must be bound to `this` if they use `this` inside them, otherwise `this` will refer to the event
-		
-	this.status=function(){//function that returns what the inventory printout should say about the this object, default is just the name
+	}).bind(this));
+	
+	this.status=function(){
 		return this.name +" - "+ this.durability;
 	};
 		
-	//events can be added externally (ie in content files) and then fired from here using
+	
 	this.emit("event-name");
-	//example
+	
 	this.damage+=Player.damage;
-	this.on("use", (function(){//(multiple events w/ same name is ok)
+	this.on("use", (function(){
 		if (this.durability<=0){
-			this.emit("broken");//deal with this event elsewhere	
+			this.emit("broken");	
 		}
 	}).bind(this));
 };
